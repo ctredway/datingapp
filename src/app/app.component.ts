1c13d16e2c1054821a48b5f023fba43654455e18
Clint Tredway <grumpee@gmail.com>
2023-03-24 16:10:44 -0500

    client additions and changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {LoginServiceService} from "./_services/login-service.service";
+import {User} from "./models/User";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit {
+  title = 'Users';
+
+
+  constructor(private http: HttpClient, private loginSrvc: LoginServiceService)
+  {
+
+  }
+
+  ngOnInit(){
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userStr = localStorage.getItem('user');
+    if(!userStr) return;
+    const user: User = JSON.parse(userStr);
+    this.loginSrvc.setCurrentUser(user);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LatestUsersComponent } from './components/latest-users/latest-users.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    LatestUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
